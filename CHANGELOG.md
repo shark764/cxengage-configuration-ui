4146e80927e47dafe91c6c3b9924473196dc3f6a
Josh Williams <tooeasytospell@gmail.com>
2020-01-17 12:00:08 -0400

    Chat Widget - App dropdown validation fix (#531)
    * App dropdown validation fix

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -28,6 +28,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Dispositions
 - Disposition Lists
 
+## [0.70.15] - 2020-01-17
+## Fixed
+- CXV1-21374 - App dropdown validation is from contact point field.
+
 ## [0.70.14] - 2020-01-14
 ## Added
 - <no-jira> - Added Email Templates automation.
--- src/containers/Form/ChatWidgets/validation.js
+++ src/containers/Form/ChatWidgets/validation.js
@@ -7,5 +7,5 @@ import { isEmpty } from 'serenova-js-utils/strings';
 export const formValidation = values => ({
   name: isEmpty(values.get('name')) && 'Please enter a name',
   contactPoint: isEmpty(values.get('contactPoint')) && 'Please enter a contact point',
-  appId: isEmpty(values.get('contactPoint')) && 'Please choose an app',
+  appId: isEmpty(values.get('appId')) && 'Please choose an app'
 });
